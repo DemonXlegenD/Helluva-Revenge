49cfc7ebcf86eba9a3b729fd877744e6a4ce4ae7
P0izon <quentinleforestier0@gmail.com>
2023-12-06 21:17:37 +0100

    faire jump

--- GameObjectLib/include/BuildersGameObject/BuilderShapeGameObject.h
+++ GameObjectLib/include/BuildersGameObject/BuilderShapeGameObject.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "GameObject.h"
+
 class BuilderShapeGameObject
 {
+	static GameObject* CreatePlatformeGameObject(const std::string& _name, const float& _scalex, const float& _scaley);
 };
 
